8d1798804abf588cb05a4fecbdf3a5fcae2cfef0
maeberli <marco.aeberli@rae.ch>
2013-10-27 12:51:59 +0100

    MOD: adapted initialization of mycolor and advcolor(depends now on player id which is passed as argument to the constructoe.)

--- tournoi/Participants/MAEDKU/Joueur.java
+++ tournoi/Participants/MAEDKU/Joueur.java
@@ -6,29 +6,48 @@ public class Joueur extends Othello.Joueur
 {
 	private Grid grid;
 	private Cell.Owner o;
-	private static final Cell.Owner MYCOLOR = Cell.Owner.RED;
-	private static final Cell.Owner ADVCOLOR = Cell.Owner.BLUE;
+	private boolean colorInitialized;
+	private Cell.Owner mycolor;
+	private Cell.Owner advcolor;
 	
 	public Joueur(int depth, int playerID) {
 		super(depth, playerID);
 		grid = new Grid(8);
+		grid.getCell(3,3).setOwner(Cell.Owner.BLUE);
+		grid.getCell(4,3).setOwner(Cell.Owner.RED);
+		grid.getCell(3,4).setOwner(Cell.Owner.RED);
+		grid.getCell(4,4).setOwner(Cell.Owner.BLUE);
+		
+		// as default, the adversary takes RED, he will be the beginner.
+		mycolor = Cell.Owner.BLUE;
+		advcolor = Cell.Owner.RED;
+		
+		if(playerID == 0)
+		{
+			// I will take the first turn, so i take RED as Color.
+			mycolor = Cell.Owner.RED;
+			advcolor = Cell.Owner.BLUE;
+		}
 	}
 
 	public Move nextPlay(Move move)
 	{
 		if(move!=null)
-			grid.addTurn(move, ADVCOLOR);
+			grid.addTurn(move, advcolor);
 		
 		Move mymove = minmax();
 		if(mymove!=null)
-			grid.addTurn(mymove, MYCOLOR);
+			grid.addTurn(mymove, mycolor);
 		
 		return mymove;
 	}
 	
 	private Move minmax()
 	{
-		MinMaxResult res = max(new Node(this.grid, MYCOLOR), this.depth);
+		Node node = new Node(this.grid, mycolor);
+		System.out.println(node.ops());
+		
+		MinMaxResult res = max(new Node(this.grid, mycolor), this.depth);
 		return res.getMove();
 	}
 	
